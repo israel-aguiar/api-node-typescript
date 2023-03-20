ba9260d0621a07e15e6cc26f2d53bb8071e325d8
Israel <israel.aguira@gmail.com>
2023-03-19 21:02:34 -0300

    API Rest, Node e Typescript: #10 - Tradução do Yup e validação de múltiplos erros

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,27 +1,41 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 
 
 interface ICidade {
-  nome: string
+  nome: string,
+  estado: string
 }
 
 const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
 
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 
-});
+}).noUnknown();
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
+    validatedData = await bodyValidation.validate(req.body, { abortEarly: false, strict: true });
+
   } catch (error) {
     const yupError = error as yup.ValidationError;
+    const validationErrors: Record<string, string> = {};
 
-    return res.json({
-      errors: yupError.message,
+    yupError.inner.forEach(error => {
+      //if (error.path) return;
+      if (error.path === undefined) return;//mesma coisa
+      
+      validationErrors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: validationErrors,
+      }
     });
   }
   
